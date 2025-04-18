9d621507612699f1db4a99af919d8bdf32cee96d
Vic2r <vic3r.brixen@gmail.com>
2025-04-18 08:07:13 +0200

    Make the maze structure

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
-#include "headers/test.h"
 #include <stdio.h>
+#define MAZEWIDTH 6
+#define MAZEHEIGHT 6
+
+typedef struct node {
+  char visited;
+  struct node *children[4];
+} node;
+
+node maze[MAZEHEIGHT][MAZEWIDTH];
+
+void print_maze() {
+  printf("Current state of the maze:\n");
+  for (int row = 0; row < MAZEHEIGHT; row++) {
+    printf("{ ");
+    for (int col = 0; col < MAZEWIDTH; col++) {
+      char *format = "%d, ";
+      if (col == MAZEWIDTH - 1) {
+        format = "%d ";
+      }
+      printf(format, maze[row][col].visited);
+    }
+    printf("}\n");
+  }
+}
 
 int main() {
-  printf("Hello World!\n");
-  printf("%d\n", number);
+  print_maze();
   return 0;
 }
