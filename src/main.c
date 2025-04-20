8ab6bdad3feedb866923ae8472372c441ab9a9ae
Vic2r <victorbrixen@proton.me>
2025-04-20 08:53:51 +0200

    Maze can be printed

--- src/main.c
+++ src/main.c
@@ -1,26 +1,63 @@
+#include <stdbool.h>
 #include <stdio.h>
-#define MAZEWIDTH 6
-#define MAZEHEIGHT 6
+#define MAZEWIDTH 20
+#define MAZEHEIGHT 10
 
 typedef struct node {
-  char visited;
-  struct node *children[4];
+  bool visited;
+  bool lower;
+  bool right;
 } node;
 
 node maze[MAZEHEIGHT][MAZEWIDTH];
 
 void print_maze() {
+
   printf("Current state of the maze:\n");
-  for (int row = 0; row < MAZEHEIGHT; row++) {
-    printf("{ ");
-    for (int col = 0; col < MAZEWIDTH; col++) {
-      char *format = "%d, ";
-      if (col == MAZEWIDTH - 1) {
-        format = "%d ";
-      }
-      printf(format, maze[row][col].visited);
+  printf("|");
+  for (int col = 0; col < MAZEWIDTH; col++) {
+    if (col != MAZEWIDTH - 1) {
+      printf("--");
+    } else {
+      printf("-");
     }
-    printf("}\n");
+  }
+
+  printf("|");
+  printf("\n");
+  for (int row = 0; row < MAZEHEIGHT; row++) {
+    printf("|");
+    for (int col = 0; col < MAZEWIDTH; col++) {
+      node *current = &maze[row][col];
+      if (!current->visited) {
+        printf(" ");
+      } else {
+        printf("#");
+      }
+      current->right = 1;
+      if (current->right) {
+        printf("|");
+      } else {
+        printf(" ");
+      }
+    }
+
+    printf("\n");
+    printf("|");
+    for (int col = 0; col < MAZEWIDTH; col++) {
+      node *current = &maze[row][col];
+      current->lower = 1;
+      if (current->lower) {
+        printf("-");
+      } else {
+        printf(" ");
+      }
+      if (col != MAZEWIDTH - 1) {
+        printf("-");
+      }
+    }
+    printf("|");
+    printf("\n");
   }
 }
 
